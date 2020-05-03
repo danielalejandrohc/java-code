378af24fa304d653d6802236e4ab4a87aa1a6fd0
danielalejandrohc <daniel.alejandro.hc@gmail.com>
2020-05-03 12:37:19 -0700

    Update PrimeNumbers.java

--- src/main/java/PrimeNumbers.java
+++ src/main/java/PrimeNumbers.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// https://projecteuler.net/problem=3
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+ */
 public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        List<Integer> primes = findPrimes(13195  );
+        System.out.printf("Result " + primes);
+    }
+
+    public static List<Integer> findPrimes(Integer number) {
+        List<Integer> ret = new ArrayList<>();
+        for(int i = 1; i <= number; i++) {
+            if ((number % i) == 0 && isPrime(i)) {
+                ret.add(i);
+            }
+        }
+        return ret;
+    }
+
+    public static boolean isPrime(Integer number) {
+        int count = 0;
+        for(int i = 1; i <= number; i++) {
+            if((number % i) == 0) {
+                count++;
+            }
+        }
+        return count == 2;
+    }
+
 }
